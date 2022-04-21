361c38c64cf56b37f9ac550166ceba9a77e3190d
YuyaNakamura0139 <mtk371@icloud.com>
2022-04-21 18:32:01 +0900

    #28 ディレクトリ構成修正やdocstringの記述をした

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ from decouple import config
 
 ACCESS_TOKEN = config("LINE_CHANNEL_ACCESS_TOKEN")
 SECRET = config("LINE_CHANNEL_SECRET")
+MONGO_API_KEY = config("MONGO_API_KEY")
 GROUP_ID = config("GROUP_ID")
--- db/db_session.py
+++ db/db_session.py
@@ -1,15 +1,16 @@
-from decouple import config
 from pymongo import MongoClient
 
+from config import MONGO_API_KEY
 
-MONGO_API_KEY = config("MONGO_API_KEY")
 client = MongoClient(MONGO_API_KEY)
 database = client.LINE_BOT
 collection_user = database.user
 
 
 def db_register(user_id: str) -> dict:
-    """ユーザーのセッション登録・取得"""
+    """
+    ユーザー情報の登録・取得
+    """
     overlap_user = collection_user.find_one({"user_id": user_id})
     if overlap_user:
         return overlap_user
@@ -30,28 +31,37 @@ def db_register(user_id: str) -> dict:
 
 
 def db_update_context(user_id: str, context: str) -> None:
-    """contextのアップデート"""
+    """
+    contextのアップデート
+    """
+
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one({"user_id": user_id}, {"$set": {"context": context}})
 
 
 def db_update_day(user_id: str, day: str) -> None:
-    """dayのアップデート"""
+    """
+    dayのアップデート
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one({"user_id": user_id}, {"$set": {"day": day}})
 
 
 def db_update_time(user_id: str, time: str) -> None:
-    """timeのアップデート"""
+    """
+    timeのアップデート
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one({"user_id": user_id}, {"$set": {"time": time}})
 
 
 def db_add_practice_name(user_id: str, practice_name: str) -> None:
-    """practice_nameへの追加"""
+    """
+    practice_nameへの追加
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one(
@@ -60,7 +70,10 @@ def db_add_practice_name(user_id: str, practice_name: str) -> None:
 
 
 def db_add_practice_time(user_id: str, practice_time: str) -> None:
-    """practice_timeへの追加"""
+    """
+    practice_timeへの追加
+    """
+
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one(
@@ -69,7 +82,9 @@ def db_add_practice_time(user_id: str, practice_time: str) -> None:
 
 
 def db_update_last_sentence(user_id: str, last_sentence: str) -> None:
-    """last_sentenceの更新"""
+    """
+    last_sentenceの更新
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one(
@@ -78,14 +93,18 @@ def db_update_last_sentence(user_id: str, last_sentence: str) -> None:
 
 
 def db_add_url(user_id: str, url: str) -> None:
-    """urlの追加"""
+    """
+    urlの追加
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.update_one({"user_id": user_id}, {"$push": {"url": url}})
 
 
 def db_reset_status(user_id: str) -> None:
-    """ユーザーのstatusの初期化"""
+    """
+    ユーザーのstatusの初期化
+    """
     user = collection_user.find_one({"user_id": user_id})
     if user:
         collection_user.delete_one({"user_id": user_id})
